7f8e6a8785798c369e321da3c9dc693b672ce68a
Annemarii Hunt <annemarii.hunt@parnulv.sise>
2018-10-20 12:18:12 +0300

    Pealkiri Artiklid, kasutades pug-i

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //õpin ja vaatan https://www.tutorialspoint.com/nodejs/nodejs_repl_terminal.htm
-
+/*
 var http = require("http");
 
 http.createServer(function (request, response) {
@@ -14,4 +14,33 @@ http.createServer(function (request, response) {
  
  // Console will print the message
  console.log('Server running at http://127.0.0.1:8081/');
+*/
+
+
+const express = require('express');
+const port = 3000;
+const path = require('path');
+
+//Init app
+const app = express()
+
+ //Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+ //Home route
+  app.get('/', (req, res) => res.render('index',{
+
+    title: 'Artiklid'
+  }))
+ 
+//add route
+
+
+
+
+
+ //Start server
+ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
 
