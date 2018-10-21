b6a6f945caf44da2400612da40d029c17cdef10e
Annemarii Hunt <annemarii.hunt@parnulv.sise>
2018-10-21 12:47:49 +0300

    Nüüd saab uuendada ja kustutada artikkleid. Parandasin mõned bugid ka

--- main.js
+++ main.js
@@ -81,6 +81,9 @@ app.get('/articles/add', function(req, res){
       })
 });
 
+
+
+
 //add sutmin POST route
 
 
@@ -98,7 +101,58 @@ res.redirect('/');
 
 }
 });
+});
+
+
+//Load edit form
+app.get('/article/edit/:id', function(req, res){
+    Article.findById(req.params.id, function(err, article){
+        res.render('edit_article', {
+            title: 'Artikli muutmine',
+            article:article
+          });
+
+});
+
+});
+
+//update Submit Post Route
+app.post('/articles/edit/:id', function(req, res){
+    let article ={};
+    article.title=req.body.title;
+    article.author=req.body.author;
+    article.body=req.body.body;
+
+    let query = {_id:req.params.id} ;
+
+    Article.update(query, article, function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+    res.redirect('/');
+    
+    }
+    });
+    });
+    
+     
+    
+
+//Delete article
+app.delete('/article/:id', function(req,res){
+let query = {_id:req.params.id}
+Article.remove(query, function(err){
+    if(err){
+console.log(err);
+
+    }
+    res.send('Success')
+
 })
+
+});
+
 
 
 
---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+console.log("Hello");
+
+$(document).ready(function(){
+$('.delete-article').on('click', function(e){
+$target = $(e.target);
+const id = $target.attr('data-id');
+$.ajax({
+    type: 'DELETE',
+    url: '/article/'+id,
+      success: function(response){
+    alert('Oled kindel, et kustutad artiklit?');
+    window.location.href='/';
+},
+    error:function(err){
+
+        console.log(err);
+} });
+
+
+});
+
+}); 
